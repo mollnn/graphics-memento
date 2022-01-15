b7d46b7972aff6a88fe403d21e7c57eeee4464da
mollnn <1084918073@qq.com>
2022-01-15 10:49:09 +0800

    perf: numba.jit for interact speed

--- raster2d_interact.py
+++ raster2d_interact.py
@@ -2,15 +2,18 @@ import sys
 import pygame
 import numpy as np
 from matplotlib import pyplot as plt
+from numba import jit
 
 t = 0
 p = np.array([[10, 10, 0], [30, 20, 0], [20, 30, 0]])
 
 
-def render(a, img_w, img_h):
-    global p
-    img = [[np.sign(np.cross(p[1]-p[0], p[0]-[j, i, 0])[2]) == np.sign(np.cross(p[2]-p[1], p[1]-[j, i, 0])[2])
-            == np.sign(np.cross(p[0]-p[2], p[2]-[j, i, 0])[2]) for j in range(img_w)] for i in range(img_h)]
+@jit(nopython=True)
+def render(a, p, img_w, img_h):
+    img = np.array([[0.0 for j in range(img_w)] for i in range(img_h)])
+    for i in range(img_h):
+        for j in range(img_w):
+            img[i][j] = 1.0 if np.sign(np.cross(p[1]-p[0], p[0]-np.array([j, i, 0.]))[2]) == np.sign(np.cross(p[2]-p[1], p[1]-np.array([j, i, 0.]))[2]) == np.sign(np.cross(p[0]-p[2], p[2]-np.array([j, i, 0.]))[2]) else 0.0
     for x in range(img_w):
         for y in range(img_h):
             a[x][y] = img[y][x] * 255
@@ -23,7 +26,7 @@ pygame.init()
 screen = pygame.display.set_mode((img_w, img_h))
 a = pygame.surfarray.pixels3d(screen)
 while True:
-    render(a, img_w, img_h)
+    render(a, p, img_w, img_h)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
